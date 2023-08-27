7ff10c307e6adc68759698d0e55e4c91b5e18ea9
Alisa Kuzmina <30773217+arisuaiko@users.noreply.github.com>
2023-08-27 16:43:05 +0200

    Adding second negative test

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,8 +15,9 @@ public class NegativeTests {
         System.out.println("Starting negativeUsernameTest");
 
         //Create driver
+        GeckoDriverService service = new GeckoDriverService.Builder().withLogOutput(System.out).build();
         System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new FirefoxDriver(service);
 
         //maximize browser window
         driver.manage().window().maximize();
@@ -44,6 +46,49 @@ public class NegativeTests {
         //unsuccessful login message
         WebElement unSuccessMessage = driver.findElement(By.cssSelector("div#flash"));
         String expectedMessage = "Your username is invalid!";
+        String actualMessage = unSuccessMessage.getText();
+        Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain expected message.\nActual message: " + actualMessage + "\nExpected Message: " + expectedMessage);
+
+        //close browser
+        driver.quit();
+    }
+
+    @Test
+    public void negativePasswordTest(){
+        System.out.println("Starting negativePasswordTest");
+
+        //Create driver
+        GeckoDriverService service = new GeckoDriverService.Builder().withLogOutput(System.out).build();
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+        WebDriver driver = new FirefoxDriver(service);
+
+        //maximize browser window
+        driver.manage().window().maximize();
+
+        //open test page
+        String url = "https://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened");
+
+        //enter username
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+        //enter password
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecret");
+        //click login button
+        WebElement logInButton = driver.findElement(By.tagName("button"));
+        logInButton.click();
+
+        //verifications:
+        //same url
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualUrl, url, "Actual page URL is not the same as expected");
+
+        //unsuccessful login message
+        WebElement unSuccessMessage = driver.findElement(By.cssSelector("div#flash"));
+        String expectedMessage = "Your password is invalid!";
         String actualMessage = unSuccessMessage.getText();
         Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain expected message.\nActual message: " + actualMessage + "\nExpected Message: " + expectedMessage);
 
